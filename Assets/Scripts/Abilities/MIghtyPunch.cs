2bad73f0117459a58710e09b78d8a417155ce103
Yockomo <Funohs@yandex.ru>
2022-05-20 17:50:09 +0300

    AbilitiesConfig

--- Assets/Scripts/Abilities/MIghtyPunch.cs
+++ Assets/Scripts/Abilities/MIghtyPunch.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAbilities : MonoBehaviour
+public class MIghtyPunch : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
