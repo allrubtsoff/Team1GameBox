462f974c8026ef910df2fb616d356707559f207a
Demionchek <Dpcustom@yandex.ru>
2022-05-26 15:28:39 +0300

    Trow Damage Added

--- Assets/Scripts/Hero/AxeThrow/AxeReturn.cs
+++ Assets/Scripts/Hero/AxeThrow/AxeReturn.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using UnityEngine;
 
-public class AxeReturn: MonoBehaviour
+public class AxeReturn : MonoBehaviour
 {
+    [SerializeField] private PlayerAbilitiesConfigs configs;
     [SerializeField] private float timeToRelocateAfterCollision;
     [SerializeField] private Transform playersHand;
     [SerializeField] private LayerMask playersLayer;
+    [SerializeField] private LayerMask enemyLayer;
 
+    private int counter = 0;
     private Rigidbody rigidBody;
 
     private void Start()
@@ -21,11 +24,18 @@ public class AxeReturn: MonoBehaviour
             rigidBody.isKinematic = true;
             StartCoroutine(ReturnAxe());
         }
+        if (collision.transform.TryGetComponent(out IDamageable damageable) && counter ==0)
+        {
+            counter++;
+            damageable.TakeDamage(configs.axeThrowDmg, enemyLayer);
+            gameObject.SetActive(false);
+        }
     }
 
     private IEnumerator ReturnAxe()
     {
         yield return new WaitForSeconds(timeToRelocateAfterCollision);
+        counter = 0;
         gameObject.SetActive(false);
         gameObject.transform.parent = playersHand;
         gameObject.transform.localPosition = new Vector3(0, 0, 0);
